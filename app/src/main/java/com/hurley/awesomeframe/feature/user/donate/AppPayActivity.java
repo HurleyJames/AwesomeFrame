0fe53dfb2110df630bdf8c890216f64625d52402
HurleyJames <1401682479@qq.com>
2019-05-28 12:28:47 +0800

    初步完成捐赠界面

---
+++ app/src/main/java/com/hurley/awesomeframe/feature/user/donate/AppPayActivity.java
@@ -0,0 +1,138 @@
+package com.hurley.awesomeframe.feature.user.donate;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.view.View;
+import android.widget.Button;
+
+import com.blankj.utilcode.util.LogUtils;
+import com.blankj.utilcode.util.ToastUtils;
+import com.hurley.awesomeframe.R;
+import com.hurley.awesomeframe.base.activity.BaseActivity;
+import com.king.pay.alipay.AliPay;
+import com.king.pay.apppay.AppPay;
+import com.king.pay.wxpay.wxapi.WXPayActivity;
+
+import butterknife.BindView;
+import butterknife.OnClick;
+
+/**
+ * <pre>
+ *      @author hurley
+ *      date    : 2019-05-28 10:09
+ *      github  : https://github.com/HurleyJames
+ *      desc    : AppPay框架页面
+ * </pre>
+ */
+public class AppPayActivity extends BaseActivity {
+
+    @BindView(R.id.btn_alipay)
+    Button mBtnAlipay;
+    @BindView(R.id.btn_wxpay)
+    Button mBtnWxpay;
+    @BindView(R.id.btn_ali_auth)
+    Button mBtnAliAuth;
+
+    AppPay mAppPay;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.app_pay_activity;
+    }
+
+    @Override
+    protected int getTitleId() {
+        return R.id.tb_app_pay;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initEventAndData() {
+        mAppPay = new AppPay(this);
+        //支付宝支付监听
+        mAppPay.setOnAliPayListener((isSuccess, resultInfo) -> {
+            if (isSuccess) {
+                // TODO 支付宝支付成功
+                // 务必以服务端结果为准
+            }
+        });
+
+        mAppPay.setOnAliPayAuthListener((isSuccess, resultInfo) -> {
+            if (isSuccess) {
+                // TODO 支付宝授权成功
+
+            }
+        });
+    }
+
+    @OnClick({R.id.btn_alipay, R.id.btn_wxpay, R.id.btn_ali_auth})
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_alipay:
+                break;
+            case R.id.btn_wxpay:
+                break;
+            case R.id.btn_ali_auth:
+                break;
+            default:
+                break;
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        unregisterPayResultReceiver();
+        super.onDestroy();
+    }
+
+    /**
+     * 注册广播广播
+     */
+    private void registerPayResultReceiver() {
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(WXPayActivity.ACTION_WX_PAY_RESULT);
+        registerReceiver(mPayResultReceiver, filter);
+    }
+
+    /**
+     * 注销广播
+     */
+    private void unregisterPayResultReceiver() {
+        unregisterReceiver(mPayResultReceiver);
+    }
+
+    /**
+     * 支付结果广播接收
+     */
+    private BroadcastReceiver mPayResultReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            int code = intent.getIntExtra(WXPayActivity.KEY_CODE, 0);
+            String error = intent.getStringExtra(WXPayActivity.KEY_ERROR);
+            LogUtils.d("code=" + code + ",error=" + error);
+            // 接收到客户端支付结果，这里一般与服务器同步支付结果，以服务器端的接收的支付通知或查询API返回的结果为准
+            switch (code) {
+                case 0:
+                    // 成功
+                    ToastUtils.showShort(R.string.pay_success);
+                    break;
+                case -1:
+                    // 错误
+                    ToastUtils.showShort(R.string.pay_error);
+                    break;
+                case -2:
+                    // 用户取消
+                    ToastUtils.showShort(R.string.pay_cancel);
+                    break;
+                default:
+                    break;
+            }
+        }
+    };
+}
---
+++ app/src/main/java/com/hurley/awesomeframe/feature/user/donate/DonateActivity.java
@@ -0,0 +1,76 @@
+package com.hurley.awesomeframe.feature.user.donate;
+
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.hurley.awesomeframe.R;
+import com.hurley.awesomeframe.base.activity.BaseActivity;
+import com.hurley.awesomeframe.data.local.FrameBean;
+import com.hurley.awesomeframe.feature.found.adapter.FrameAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+
+/**
+ * <pre>
+ *      @author hurley
+ *      date    : 2019-05-28 09:51
+ *      github  : https://github.com/HurleyJames
+ *      desc    : 赞助界面
+ * </pre>
+ */
+public class DonateActivity extends BaseActivity implements BaseQuickAdapter.OnItemClickListener {
+
+    @BindView(R.id.rv_donate)
+    RecyclerView mRvDonate;
+
+    private FrameAdapter mAdapter;
+
+    private List<FrameBean> mList;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.donate_activity;
+    }
+
+    @Override
+    protected int getTitleId() {
+        return R.id.tb_donate;
+    }
+
+    @Override
+    protected void initView() {
+        mRvDonate.setLayoutManager(new LinearLayoutManager(this));
+    }
+
+    @Override
+    protected void initEventAndData() {
+        mList = new ArrayList<>();
+        mAdapter = new FrameAdapter(R.layout.frame_recycle_item, mList);
+        mRvDonate.setAdapter(mAdapter);
+        mAdapter.openLoadAnimation(BaseQuickAdapter.ALPHAIN);
+        mAdapter.isFirstOnly(false);
+        mAdapter.setNewData(setListData(mList));
+        mAdapter.setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(BaseQuickAdapter adapter, View view, int position) {
+        switch (position) {
+            case 0:
+                startActivity(AppPayActivity.class);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private List<FrameBean> setListData(List<FrameBean> list) {
+        list.add(new FrameBean("AppPay", "jenly1314", "\uD83D\uDCB8 AppPay专注于App支付的库，几句代码轻松搞定微信支付、支付宝支付。集成更简单。"));
+        return list;
+    }
+}
